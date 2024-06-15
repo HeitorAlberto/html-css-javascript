6c74194130fe54a1f3a72fb8cb083fe376292e46
HeitorAlberto <56478574+HeitorAlberto@users.noreply.github.com>
2024-06-15 12:43:07 -0300

    Modificações no script e no CSS

--- projeto-calculadora-imc/script.js
+++ projeto-calculadora-imc/script.js
@@ -9,6 +9,7 @@ let peso = document.querySelector("#cxpesoid");
 let resultadoimc = "";
 let txtResultado = document.querySelector("#txt-resultado");
 let txtResultadoMsg = document.querySelector("#txt-resultado-msg");
+let caixaResultado = document.querySelector("#caixa-resultado");
 
 let btn = document.querySelector("#btn-calcular"); //Seleciona o botão que calcula
 let btn2 = document.querySelector("#btn-limpar"); //Seleciona o botão que limpa os campos
@@ -17,39 +18,45 @@ let btn2 = document.querySelector("#btn-limpar"); //Seleciona o botão que limpa
 
 
 
+// Calculo do IMC
 function calcularIMC(){
-    
+    resultadoimc = parseFloat(peso.value) / (parseFloat(altura.value) * parseFloat(altura.value));
 
-    resultadoimc = parseFloat(peso.value) / (parseFloat(altura.value) * parseFloat(altura.value) ) // resultado do imc salvo na variável
-    
     if (resultadoimc < 18.5){
         txtResultado.innerHTML = "Resultado: " + resultadoimc.toFixed(2) + " (Magreza)"
         txtResultadoMsg.innerHTML = "Cuide-se, " + nome.value + ". Seu peso está abaixo do normal.";
-        txtResultado.style.color = "red";
-        txtResultadoMsg.style.color = "red";
+        caixaResultado.style.backgroundColor = "red";
+        
     } else if (resultadoimc >= 18.5 && resultadoimc <= 24.9){
         txtResultado.innerHTML = "Resultado: " + resultadoimc.toFixed(2) + " (Normal)"
         txtResultadoMsg.innerHTML = "Boas notícias, " + nome.value + ". Seu peso está normal.";
-        txtResultado.style.color = "green";
-        txtResultadoMsg.style.color = "green";
+        caixaResultado.style.backgroundColor = "green";
+
     } else if (resultadoimc >= 25 && resultadoimc <= 30){
         txtResultado.innerHTML = "Resultado: " + resultadoimc.toFixed(2) + " (Sobrepeso)"
         txtResultadoMsg.innerHTML = "Cuide-se, " + nome.value + ". Seu peso está acima do normal.";
-        txtResultado.style.color = "#f05d02";
-        txtResultadoMsg.style.color = "#f05d02";
+        caixaResultado.style.backgroundColor = "#f05d02";
+
     } else if (resultadoimc > 30){
         txtResultado.innerHTML = "Resultado: " + resultadoimc.toFixed(2) + " (Obesidade)"
         txtResultadoMsg.innerHTML = "Atenção, " + nome.value + ". Seu peso está muito acima do normal.";
-        txtResultado.style.color = "red";
-        txtResultadoMsg.style.color = "red";
+        caixaResultado.style.backgroundColor = "red";
+
     }
 
-    
 }
+
+        
+
+
+
 
 function limparCampos(){
     txtResultado.innerHTML = "Olá, " + saudacao + "!";
     txtResultadoMsg.innerHTML = "Prencha os campos acima para obter o seu IMC."
+    txtResultado.style.color = "black"
+    txtResultadoMsg.style.color = "black"
+    caixaResultado.style.backgroundColor = "white"
 }
 
 
@@ -69,6 +76,7 @@ if (horas < 12) {
 }
 
 txtResultado.innerHTML = "Olá, " + saudacao + "!";
+
 
 btn.addEventListener("click", function(event){event.preventDefault(); calcularIMC()});
 //Ao clicar no botão, a função anônima executa o "preventDefault()" que impede o comportamento padrão que é recarregar a página, logo em seguida chama a função "calcularIMC" responsável pelo cálculo do IMC
